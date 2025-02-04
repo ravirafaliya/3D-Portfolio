56a9511b3c4c8eb6ed7ece53b345900be045955b
DREAMWORLD <dreamworld@Ravis-Mac-Pro.local>
2025-02-04 12:47:05 +0530

    feat:folder structure

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio'>Portfolio</div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Services.css'
+
+const Services = () => {
+  return (
+    <div className='services'>Services</div>
+  )
+}
+
+export default Services
